0f94b5446e80dfc5ea1681d0292e2843c68e5965
Howard <hy4717@ic.ac.uk>
2021-05-03 14:02:32 +0100

    removed .idea, process ?

--- src/main/Simple.java
+++ src/main/Simple.java
@@ -3,132 +3,175 @@ package main;
 import se.sics.jasper.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Simple
 {
     public static void main(String[] args) {
         // read lines from predicate_definitions.txt and generate test queries
+        // then, pass them to get SLD trees
 
-
-
-
-        SICStus sp;
-        Query generateArgsQuery;
-        Query optionalGenerateArgsQuery;
-        HashMap<String, SPTerm> argsWayMap = new HashMap<>();
-        List<String> testQueries;
-
+        // First, read from file
+        File predicateDefinitions = new File("predicate_definitions.txt");
+        Scanner scanner = null;
         try {
-            File predicateDefinitions = new File("predicate_definitions.txt");
-            Scanner scanner = new Scanner(predicateDefinitions);
+            scanner = new Scanner(predicateDefinitions);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
 
-            sp = new SICStus();
-            sp.restore("generators.sav");
+        // Next, iterate through lines
+        assert scanner != null;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] split = line.split(";");
 
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] split = line.split(";");
+            String predicate = split[0];
+            String argsRestrictions = split[1];
+            String optionalArgsRestrictions = "";
+            if (split.length == 3) {
+                optionalArgsRestrictions = split[2];
+            }
 
-                String predicate = split[0];
-                String argsRestrictions = split[1];
-                String optionalArgsRestrictions = "";
-                if (split.length == 3) {
-                    optionalArgsRestrictions = split[2];
-                }
+            // generate args for + inputs
+            SICStus sp;
+            Query generateArgsQuery;
+            HashMap<String, SPTerm> argsWayMap = new HashMap<>();
+            List<List<String>> generatedArgs = null;
 
-                List<List<String>> generatedArgs = null;
-                List<List<String>> optionalGeneratedArgs = null;
-
-                System.out.println(predicate);
-                System.out.println(argsRestrictions);
-                System.out.println(optionalArgsRestrictions);
-
-                generateArgsQuery = sp.openPrologQuery(String.format("input_gen(I, %s).", argsRestrictions), argsWayMap);
-
+            try {
+                sp = new SICStus();
+                sp.restore("generators.sav");
+                generateArgsQuery = sp.openPrologQuery(String.format("input_gen(I, %s).",
+                        argsRestrictions), argsWayMap);
                 try {
                     generateArgsQuery.nextSolution();
                     generatedArgs = spTermToListOfLists(argsWayMap.get("I").toString());
                     System.out.println("Generated Arguments");
                     System.out.println(generatedArgs.toString() + "\n");
-                    System.out.println("");
                 } catch ( Exception e ) {
                     System.out.println(e.toString());
                 } finally {
                     generateArgsQuery.close();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-                if (!optionalArgsRestrictions.isEmpty()) {
+            List<List<String>> optionalGeneratedArgs = null;
+            // generate args for ? inputs if optionalArgsRestrictions exists
+            if (!optionalArgsRestrictions.isEmpty()) {
+                Query optionalGenerateArgsQuery;
+                HashMap<String, SPTerm> optionalArgsWayMap = new HashMap<>();
+
+                try {
+                    sp = new SICStus();
+                    sp.restore("generators.sav");
                     optionalGenerateArgsQuery = sp.openPrologQuery(String.format("input_gen(I, %s).",
-                            optionalArgsRestrictions), argsWayMap);
+                            optionalArgsRestrictions), optionalArgsWayMap);
                     try {
                         optionalGenerateArgsQuery.nextSolution();
-                        optionalGeneratedArgs = spTermToListOfLists(argsWayMap.get("I").toString());
+                        optionalGeneratedArgs = spTermToListOfLists(optionalArgsWayMap.get("I").toString());
                         System.out.println("Generated Arguments");
                         System.out.println(optionalGeneratedArgs.toString() + "\n");
-                        System.out.println("");
                     } catch ( Exception e ) {
                         System.out.println(e.toString());
                     } finally {
                         optionalGenerateArgsQuery.close();
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
-                testQueries = fillArguments(predicate, generatedArgs);
-                System.out.println("Test Queries");
-                for (String testQuery : testQueries) {
-                    System.out.println(testQuery);
-                }
-
-            }
-            scanner.close();
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        // iterate through all test queries and print SLD tree
-        if (args.length < 1) {
-            System.out.println("Please enter a test query");
-            return;
-        }
-        Query modelQuery;
-        HashMap<String, SPTerm> modelWayMap = new HashMap<>();
-        try {
-            sp = new SICStus();
-            sp.restore("compare_dynamic.sav");
-            String testQuery = args[0];
-            if (testQuery.charAt(testQuery.length()-1) == '.') {
-                testQuery = testQuery.substring(0, testQuery.length()-1);
             }
 
-            modelQuery = sp.openPrologQuery(String.format("solve(model:%s, P).", testQuery), modelWayMap);
+            // iterate through all test queries and print SLD tree
+            List<String> testQueries = fillArguments(predicate, generatedArgs);
+            if (optionalGeneratedArgs != null) {
+                testQueries = fillArgumentsQuestion(testQueries, optionalGeneratedArgs);
+
+            }
+            System.out.println("=====Test Queries=====");
+//            System.out.println(testQueries.size());
+            for (String testQuery : testQueries) {
+                System.out.println(testQuery);
+            }
+
+            // iterate through all test queries and print SLD tree
+            if (args.length < 1) {
+                System.out.println("Please enter a test query");
+                return;
+            }
+            Query modelQuery;
+            HashMap<String, SPTerm> modelWayMap = new HashMap<>();
             try {
-                while (modelQuery.nextSolution()) {
-                    List<List<String>> modelSLD = spTermToListOfLists(modelWayMap.get("P").toString());
-                    System.out.println(modelSLD.toString() + "\n");
+                sp = new SICStus();
+                sp.restore("compare_dynamic.sav");
+                String testQuery = args[0];
+                if (testQuery.charAt(testQuery.length()-1) == '.') {
+                    testQuery = testQuery.substring(0, testQuery.length()-1);
+                }
+
+                modelQuery = sp.openPrologQuery(String.format("solve(model:%s, P).", testQuery), modelWayMap);
+                try {
+                    while (modelQuery.nextSolution()) {
+                        List<List<String>> modelSLD = spTermToListOfLists(modelWayMap.get("P").toString());
+                        System.out.println(modelSLD.toString() + "\n");
+                    }
+                } catch ( Exception e ) {
+                    if (!e.toString().contains("permission_error")) {
+                        System.out.println(e.toString());
+                    }
+                } finally {
+                    modelQuery.close();
                 }
             } catch ( Exception e ) {
-                if (!e.toString().contains("permission_error")) {
-                    System.out.println(e.toString());
-                }
-            } finally {
-                modelQuery.close();
+                e.printStackTrace();
             }
-        } catch ( Exception e ) {
-            e.printStackTrace();
+
         }
     }
 
-    private static List<String> fillArguments(String predicate, List<List<String>> generatedArgs) {
+    private static List<String> fillArgumentsQuestion(List<String> testQueriesWithQuestion,
+                                                      List<List<String>> optionalGeneratedArgs) {
+        List<String> testQueries = new ArrayList<>();
+        char variable = 'A';
+
+        while (testQueriesWithQuestion.get(0).indexOf('?') != -1) {
+            // start fresh
+            testQueries = new ArrayList<>();
+
+            for (String testQueryWithQuestion : testQueriesWithQuestion) {
+                int index = testQueryWithQuestion.indexOf('?');
+                // replace ? with a variable, AA to AZ supported
+                testQueries.add(testQueryWithQuestion.substring(0, index) + 'A' + variable + testQueryWithQuestion.substring(index+1));
+
+                // replace ? with possible arguments
+                for (String arg : optionalGeneratedArgs.get(0)) {
+                    testQueries.add(testQueryWithQuestion.substring(0, index) + arg + testQueryWithQuestion.substring(index+1));
+                }
+            }
+
+            // remove used args and increment variable
+            variable++;
+            optionalGeneratedArgs.remove(0);
+
+            testQueriesWithQuestion = new ArrayList<>(testQueries);
+        }
+        return testQueries;
+    }
+
+    private static List<String> fillArguments(String predicate,
+                                              List<List<String>> generatedArgs) {
+
+        // replace all '-' with unique capital letters (limited from A to Z)
         char variable = 'A';
         while (predicate.indexOf('-') != -1) {
             predicate = predicate.replaceFirst("-", String.valueOf(variable));
             variable++;
-            System.out.println(predicate);
         }
 
-
+        // replace all '+' with all possible combinations of the generatedArgs
         List<String> predicates = Collections.nCopies(1, predicate);
         for (List<String> generatedArg : generatedArgs) {
             List<String> new_predicates = new ArrayList<>();
